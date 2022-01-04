6c27f5f24a1b1f393bd18e8ce061d600d93f1139
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2022-01-04 13:49:24 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'istanbul.sanat.rehberi',
+  appName: 'ARTINGEN',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
